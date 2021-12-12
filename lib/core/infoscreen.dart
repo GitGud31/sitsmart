fa50c42aca1131706ff7af7c68bc4c6f8f32289a
samitkapoor <samitkapoor77@gmail.com>
2021-12-12 23:27:07 +0530

    Added info screen text and new fonts

--- lib/core/infoscreen.dart
+++ lib/core/infoscreen.dart
@@ -4,18 +4,57 @@ import 'package:get/get.dart';
 import '/components/animatedsliverappbar.dart';
 
 class InformationScreen extends StatelessWidget {
-  const InformationScreen({Key? key}) : super(key: key);
+  InformationScreen({Key? key}) : super(key: key);
   static const routeName = "/sitsmart-info";
+
+  TextStyle lightStyle = const TextStyle(
+    color: Colors.black,
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+    letterSpacing: 1,
+    fontFamily: 'Product Sans',
+  );
+
+  TextStyle heavyStyle = const TextStyle(
+    color: Colors.black,
+    fontSize: 22,
+    fontWeight: FontWeight.bold,
+    letterSpacing: 1,
+    fontFamily: 'Product Sans',
+  );
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        backgroundColor: Get.theme.scaffoldBackgroundColor,
-        body: const CustomScrollView(
+        backgroundColor: Colors.grey,
+        body: CustomScrollView(
           physics: const BouncingScrollPhysics(),
           slivers: [
             const AnimatedSliverAppBar(),
+            SliverToBoxAdapter(
+              child: Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: RichText(
+                    textAlign: TextAlign.justify,
+                    text: TextSpan(
+                      children: <TextSpan>[
+                        TextSpan(text: 'sitsmart', style: heavyStyle),
+                        TextSpan(
+                            text:
+                                ' is an application that is designed to remind you the',
+                            style: lightStyle),
+                        TextSpan(
+                            text: ' importance of taking a break',
+                            style: heavyStyle),
+                        TextSpan(
+                            text:
+                                ' after every hour of sitting in front of your laptop. this application is made for those who spend most of their day in front of their laptop, computer or mobile screen and do not take frequent breaks. this application comes with a timer that you can start with just a single press and after the set limit, it will remind you to stand up and take a break for 10 minutes. ',
+                            style: lightStyle),
+                      ],
+                    ),
+                  )),
+            ),
           ],
         ),
       ),
